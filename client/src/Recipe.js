84f5e644c4f5f1ce664c66bbfd4ba66b25556884
beepritchard <sirin15@mac.com>
2020-03-30 19:22:15 -0500

    add edited files

--- client/src/Recipe.js
+++ client/src/Recipe.js
@@ -1,16 +1,18 @@
 import React from "react";
 import style from "././recipe.module.css";
 
-const Recipe = ({title,calories,image, ingredients}) => {
+const Recipe = ({title,dietLabels,calories,image, ingredients}) => {
     return (
         <div className={style.recipe}>
             <h3>{title}</h3>
+            <p>{dietLabels}</p>
+            <h6>Ingredients: </h6>
             <ol>
                 {ingredients.map(ingredient => (
                     <li>{ingredient.text}</li>
                 ))}
             </ol>
-            <p>{calories}</p>
+            <p>Calories: {parseInt(calories)} kcal</p>
             <img className={style.image} src={image} alt="" />
         </div>
     );
