cba01a389d18ee1a358ab7b45ea3bd7fcaf5212d
MHane952 <haneymason22@gmail.com>
2020-04-01 14:27:40 -0500

    Calories ParseInt and Diet Types displaying

--- client/src/components/Text/index.js
+++ client/src/components/Text/index.js
@@ -1,18 +1,19 @@
 import React from "react";
 import "./style.css";
 
-const Recipe = ({title,calories,image,ingredients,}) => {
+const Recipe = ({title, dietLabels, calories,image,ingredients,}) => {
     return (
        <div className= "recipe">
            <h3>{title}</h3>
-           <p>Ingredients</p>
+           <p>Diet Type: {dietLabels}</p>
+           <p>Ingredients:</p>
            <ol>
                {ingredients.map(ingredient => (
                    
                    <li>{ingredient.text}</li>
                ))}
            </ol>
-           <p>Calories: {calories}</p>
+           <p>Calories: {parseInt(calories)}</p>
            <img className= "image" src={image} alt="" />
            <div>
             <button className="btn badge-pill btn-outline-warning mt-3 ml-3" >
