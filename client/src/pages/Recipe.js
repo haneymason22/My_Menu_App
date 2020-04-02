3054d76ee7f61357ad9f8955cdcb66cc19cdbc76
MHane952 <haneymason22@gmail.com>
2020-04-01 20:07:12 -0500

    deployment3

--- client/src/pages/Recipe.js
+++ client/src/pages/Recipe.js
@@ -17,7 +17,7 @@ const Recipe = () => {
     
        const getRecipes = async () => {
          const response = await fetch(
-           `https:api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+           `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
          );
          const data = await response.json();
          setRecipes(data.hits);
